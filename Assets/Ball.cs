566ea0a2dd45f1f5a931e882bc4a75dfbe550c96
Lukas <lukas.almgren03@gmail.com>
2023-09-01 16:39:54 +0200

    Collision with walls

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,32 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private bool sent = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 1;
+    private Vector3 direction;
 
     // Update is called once per frame
     void Update()
     {
-        if(!sent)
+        if (!sent)
+        {
             if(Input.GetButton("Fire1"))
                 SendIt();
+        }
+        else
+        {
+            transform.position += speed * Time.deltaTime * direction;
+        }
     }
 
     private void SendIt()
     {
         transform.parent = null;
         sent = true;
+        direction = new Vector3(1, 1, 0).normalized;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        direction = Vector3.Reflect(direction.normalized, col.contacts[0].normal);
     }
 }
